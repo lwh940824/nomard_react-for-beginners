f838367fb0eeac42cde03ac7b3810c39ca3b80f2
lwh940824 <lwh940824@gmail.com>
2022-12-11 16:40:48 +0900

    git 배포

---
+++ src/componets/Movie.js
@@ -0,0 +1,26 @@
+import PropsTypes from "prop-types"
+import { Link } from "react-router-dom";
+function Movie({ id, coverImg, title, summary, genres }) {
+    return (
+        <div>
+            <img src={coverImg} alt={title}/>
+            <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+Movie.prototype = {
+    id: PropsTypes.number.isRequired,
+    coverImg: PropsTypes.string.isRequired,
+    title: PropsTypes.string.isRequired,
+    summary: PropsTypes.string.isRequired,
+    genres: PropsTypes.arrayOf(PropsTypes.string).isRequired
+}
+
+export default Movie;
